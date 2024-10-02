82f1925c8673131bc058a315adac360bd1b3ae8d
khaynaura <146971893+khaynaura@users.noreply.github.com>
2024-10-02 13:32:47 +0700

    fixing login user

--- main/views.py
+++ main/views.py
@@ -75,14 +75,16 @@ def register(request):
 
 def login_user(request):
    if request.method == 'POST':
-      form = AuthenticationForm(data=request.POST)
-
-      if form.is_valid():
-        user = form.get_user()
-        login(request, user)
-        response = HttpResponseRedirect(reverse("main:show_main"))
-        response.set_cookie('last_login', str(datetime.datetime.now()))
-        return response
+        form = AuthenticationForm(data=request.POST)
+      
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.error(request, "Invalid username or password. Please try again.")
 
    else:
       form = AuthenticationForm(request)
